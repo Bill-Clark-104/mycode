c66f502d9970a02e527bd8c46cacbdad0db00bd3
Bill-Clark-104 <clark2519@gmail.com>
2024-10-29 20:16:41 +0000

    terraform null resource loops

--- docker-challenge/main.tf
+++ docker-challenge/main.tf
@@ -14,12 +14,12 @@ provider "docker" {}
 
 resource "docker_image" "simplegoservice" {
   name         = "registry.gitlab.com/alta3/simplegoservice"
-  keep_locally = true      // keep image after "destroy"
+  keep_locally = true // keep image after "destroy"
 }
 
 resource "docker_container" "simplegoservice" {
   image = docker_image.simplegoservice.image_id
-  name = var.container_name
+  name  = var.container_name
   ports {
     internal = var.internal_port
     external = var.external_port
